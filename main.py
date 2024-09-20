81dcc4d833109a3ccbca57fbe94a6d306a76a16e
nianwei100 <2297709907@qq.com>
2024-09-20 12:24:11 +0800

    re

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from random import randint
+from __future__ import barry_as_FLUFL
 
-from rich import print
+from functools import reduce
+from random import randint
+from typing import Dict, List, NewType, Optional, Set, Tuple, Union
+
+import pretty_errors
 from rich.highlighter import Highlighter
+from rich.text import Text
+
+pretty_errors.configure(display_link=True)
+
+
+class RainbowHighlighter(Highlighter):
+
+    def highlight(self, text: Text) -> None:
+        for index in range(len(text)):
+            text.stylize(f"color({randint(16, 255)})", index, index + 1)
 
 
 class Microware:
@@ -39,13 +53,56 @@ class Microware:
         return f'Microwave(brand="{self.brand}", power_rating="{self.power_rating}")'
 
 
-smeg: Microware = Microware('Smeg', 'B')
-bosch: Microware = Microware('Bosch', 'C')
-samsung: Microware = Microware('Samsung', 'A')
+# smeg: Microware = Microware('Smeg', 'B')
+# bosch: Microware = Microware('Bosch', 'C')
+# samsung: Microware = Microware('Samsung', 'A')
 
 
-class RainbowHighlighter(Highlighter):
+def greeting(name: Optional[str]) -> str:
+    return f'Hello {name}'
 
-    def highlight(self, text) -> None:
-        for index in range(len(text)):
-            text.stylize(f"color({randint(16, 255)})", index, index + 1)
+
+def double_or_square(number: Union[int, float]) -> Union[int, float]:
+    if isinstance(number, int):
+        return number * 2
+    else:
+        return number**2
+
+
+UserId = NewType('UserId', int)
+some_id = UserId(1243)
+
+
+def add(x: int, y: int) -> int:
+    return x + y
+
+
+num_list = [i for i in range(1, 101)]
+result = reduce(add, num_list)
+
+
+def factorial(n):
+    assert n >= 0, 'n should be greater than 0'
+    if n == 0 or n == 1:
+        return 1
+    return reduce(lambda a, b: a * b, range(1, n + 1))
+
+
+def get_name_age() -> Tuple[str, int]:
+    return '王五', 18
+
+
+def get_students() -> List[str]:
+    return ["小黑", "小华", "小米"]
+
+
+def get_scores() -> Dict[str, int]:
+    return {"python": 100, "java": 80, "go": 80}
+
+
+def get_unique_numbers() -> Set[Union[str, int]]:
+    return {1, 2, 3, 4, 5, 6, '7'}
+
+
+if __name__ == '__main__':
+    pass
