451e8f8623b8c4618b3aa8d43dd860de224c7099
nianwei <2297709907@qq.com>
2024-10-03 16:42:31 +0800

    Run root

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Check if user is root
+# Check if user is root
 if [ "$(id -u)" != "0" ]; then
   echo "Error: You must be root to run this script, please use root to install"
   exit 1
